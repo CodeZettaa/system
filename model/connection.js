936f7631cce300d814d232b8abfdc000bad01a30
Nourhan <nourhan.saeed@almatar.com>
2025-07-17 02:58:07 +0300

    handle uniqu

--- model/connection.js
+++ model/connection.js
@@ -2,9 +2,7 @@ import mongoose from "mongoose";
 
 const connect = () => {
   mongoose
-    .connect(
-      "mongodb+srv://systemAdmin:system@cluster0.lri9y.mongodb.net/system"
-    )
+    .connect("mongodb+srv://systemAdmin:system@cluster0.lri9y.mongodb.net")
     .then((res) => console.log("connetc"));
 };
 
