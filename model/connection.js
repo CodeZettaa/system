3f05f45e22d7b3d9d769dc0fb223b30422bfe021
Nourhan <nourhan.saeed@almatar.com>
2025-07-17 03:03:46 +0300

    handle db

--- model/connection.js
+++ model/connection.js
@@ -1,9 +1,13 @@
 import mongoose from "mongoose";
+import companyModel from "./company.js";
 
 const connect = () => {
   mongoose
     .connect("mongodb+srv://systemAdmin:system@cluster0.lri9y.mongodb.net")
-    .then((res) => console.log("connetc"));
+    .then(async (res) => {
+      await companyModel.syncIndexes();
+      console.log("connected");
+    });
 };
 
 export default connect;
